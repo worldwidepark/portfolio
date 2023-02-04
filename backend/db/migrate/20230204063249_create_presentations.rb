73c52e3250773e466854519da56bf076a7ff89c5
worldwidepark <qkrguddn1066@naver.com>
2023-02-04 22:45:42 +0900

    presentationテーブル作成

---
+++ backend/db/migrate/20230204063249_create_presentations.rb
@@ -0,0 +1,10 @@
+class CreatePresentations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :presentations do |t|
+      t.references :user, null: false
+      t.references :target, null:false, polymorphic: true
+      t.boolean :present
+      t.timestamps
+    end
+  end
+end
