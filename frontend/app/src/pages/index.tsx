4217b3ea6824ff939364525eea61b72a4eb1d1cf
worldwidepark <qkrguddn1066@naver.com>
2023-03-30 11:16:19 +0900

    URL ITEM表示不具合対応

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -37,6 +37,7 @@ const index = () => {
           setProgramminglanguageTags(
             presentationElementDatas.programmingLanguageTags
           )
+          setUrlItem()
         }
       )
     }
